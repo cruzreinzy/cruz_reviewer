c61de57c3cf3e802cae67fc9ebff22556baf93a9
cruzreinzy <reinbackupacc@gmail.com>
2023-06-21 21:59:36 +0800

    other info updates

--- resources/views/others.blade.php
+++ resources/views/others.blade.php
@@ -1,3 +1,21 @@
 <!-- other infos -->
 
 <!-- database creation -> env change data base name and type php artisan migrate -->
+
+
+<!-- \App\Models\User::factory()->create([
+            'name' => 'Administrator',
+            'email' => 'admin',
+        ]);
+
+(DatabaseSeeder) -->
+
+
+<!-- return [
+    'name' => fake()->name(),
+    'username' => fake()->username(),
+    'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+    'remember_token' => Str::random(10),
+];
+
+(UserFactory) -->
